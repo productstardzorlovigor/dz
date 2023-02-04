4afceae3a9e46df2c2f54843222ed6307c484171
Игорь Орлов <test@testsymfony.test>
2023-02-05 03:03:59 +0700

    ClassAndObject4_2

--- src/ClassAndObject4Pattern/Main.java
+++ src/ClassAndObject4Pattern/Main.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    public static String email = "Ab._bc.d@Abc.Com";
+    public static String email = "_Ab._bc.d@Abc.Com";
     public static void main(String[] args) {
         System.out.println(testEmail(email));
     }
