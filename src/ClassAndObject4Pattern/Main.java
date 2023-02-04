4999c567cc7ca429c180272b30f4126b9ff0f286
Орлов Игорь Владимирович <test@testsymfony.test>
2023-02-05 03:01:47 +0700

    ClassAndObject4

---
+++ src/ClassAndObject4Pattern/Main.java
@@ -0,0 +1,19 @@
+package ClassAndObject4Pattern;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static String email = "Ab._bc.d@Abc.Com";
+    public static void main(String[] args) {
+        System.out.println(testEmail(email));
+    }
+
+    public static boolean testEmail(String email){
+        String EMAIL_PATTERN = "^[_A-Za-z0-9]+(\\.[_A-Za-z0-9]+)*@[A-Za-z0-9]+(\\.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
